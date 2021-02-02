45908477db2670df63ac26cc2df0ca8fec9e05f6
zhanghaibo <12345678>
2021-02-02 09:40:06 +0800

    第三周作业

---
+++ Week_03/nio02/src/main/java/io/github/kimmking/gateway/NettyServerApplication.java
@@ -0,0 +1,31 @@
+package io.github.kimmking.gateway;
+
+
+import io.github.kimmking.gateway.inbound.HttpInboundServer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class NettyServerApplication {
+    
+    public final static String GATEWAY_NAME = "NIOGateway";
+    public final static String GATEWAY_VERSION = "1.0.0";
+    
+    public static void main(String[] args) {
+        String proxyServer = System.getProperty("proxyServer","http://localhost:8088");
+        String proxyPort = System.getProperty("proxyPort","8888");
+        
+          //  http://localhost:8888/api/hello  ==> gateway API
+          //  http://localhost:8088/api/hello  ==> backend service
+    
+        int port = Integer.parseInt(proxyPort);
+        System.out.println(GATEWAY_NAME + " " + GATEWAY_VERSION +" starting...");
+        HttpInboundServer server = new HttpInboundServer(port, Arrays.asList("http://localhost:8801","http://localhost:8802"));
+        System.out.println(GATEWAY_NAME + " " + GATEWAY_VERSION +" started at http://localhost:" + port + " for server:" + proxyServer);
+        try {
+            server.run();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
